f26f94ae5c920fa87d05551ca7448e67b1751130
Kristijan <kristijan.pajtasev@hotmail.com>
2020-05-09 13:19:19 +0100

    dashboard component

--- src/CypressAnalytics.js
+++ src/CypressAnalytics.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Dashboard from "./components/Dashboard";
 import './CypressAnalytics.css';
+
 function CypressAnalytics() {
   return (
     <div className="CypressAnalytics">
+      <div>Cypress analytics</div>
+        <Dashboard />
     </div>
   );
 }

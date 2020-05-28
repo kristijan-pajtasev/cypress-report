e58e59ebd8623ab0b5c11e32894c76682d8b904d
Kristijan <kristijan.pajtasev@hotmail.com>
2020-05-28 20:26:29 +0100

    init results object for failed tests

--- reporter.js
+++ reporter.js
@@ -14,8 +14,29 @@ function MyReporter(runner) {
         // todo implement pass test
     }
 
-    const failTest = (test, namespace, errorMessage) => {
+    const addFailTest = (test, namespace, errorMessage, resultsObject) => {
         // todo implement pass test
+        if(namespace.length > 0) {
+            const firstNamespace = namespace[0];
+            if(!resultsObject[firstNamespace]) {
+                resultsObject[firstNamespace] = {
+                    title: firstNamespace,
+                    pass: 0,
+                    fail: 0,
+                    total: 0,
+                    tests: []
+                }
+            }
+            resultsObject[firstNamespace].fail ++;
+            resultsObject[firstNamespace].total ++;
+            addPassTest(test, namespace.slice(1), errorMessage, resultsObject[firstNamespace])
+        } else {
+            resultsObject.tests.push({
+                title: test.title,
+                passed: false,
+                message: errorMessage
+            })
+        }
     }
 
     const getTestNamespace = test => {
@@ -31,19 +52,22 @@ function MyReporter(runner) {
 
     runner.on('pass', function(test) {
         passes++;
-        console.log('testNamespace: ', getTestNamespace(test));
+        addPassTest(test, getTestNamespace(test));
+        console.log('testNamespace: ', getTestNamespace(test), results);
         console.log('pass: %s', test.parent.title);
         // console.log('pass: %s', test.fullTitle()); //
     });
 
     runner.on('fail', function(test, err) {
         failures++;
+        addFailTest(test, getTestNamespace(test), err.message, results);
         console.log('testNamespace: ', getTestNamespace(test));
         console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
     });
 
     runner.on('end', function(args) {
         console.log('end: %d/%d', passes, passes + failures);
+        console.log('result object: ', results);
     });
 }
 module.exports = MyReporter;

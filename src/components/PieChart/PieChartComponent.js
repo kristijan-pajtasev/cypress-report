be148d2424485145f585f03c15bd2f694cdcb10b
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-03 01:20:11 +0100

    pie radius

--- src/components/PieChart/PieChartComponent.js
+++ src/components/PieChart/PieChartComponent.js
@@ -5,8 +5,8 @@ function PieChartComponent(props) {
     const {data} = props;
     return (
         <div>
-            <PieChart width={500} height={250}>
-                <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8">
+            <PieChart width={400} height={400}>
+                <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={150} fill="#8884d8">
                     <Cell fill='#00C49F' />
                     <Cell fill='#FF8042' />
                 </Pie>

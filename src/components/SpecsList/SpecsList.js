2be4be50e2aa1a9c1e4ca8d1dbb509874956d3f5
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-04 02:05:18 +0100

    status icon

--- src/components/SpecsList/SpecsList.js
+++ src/components/SpecsList/SpecsList.js
@@ -5,13 +5,13 @@ const SpecsList = (props) => {
     const {specs, stats} = props;
 
     const getTestListForSpec = (spec, tests = []) => {
-        if(spec.tests && spec.tests.length > 0) tests = [...tests, spec.tests.map(test => ({
+        if (spec.tests && spec.tests.length > 0) tests = [...tests, spec.tests.map(test => ({
             title: test.fullTitle,
             passed: test.passed
         }))]
 
         const suites = Object.keys(spec).filter(key => !["tests", "pass", "fail", "total", "title", "fullTitle"].includes(key));
-        if(suites.length > 0) {
+        if (suites.length > 0) {
             suites.forEach(suite => {
                 const testsForSuite = getTestListForSpec(spec[suite]);
                 tests = [...tests, ...testsForSuite];
@@ -24,15 +24,23 @@ const SpecsList = (props) => {
     return (
         <ul className='SpecsList'>
             {specs.map((spec, index) =>
-                (<li key={`suite-${index}`} className={['SpecsList__Spec', stats[spec].fail > 0 ? 'error' : ''].join(" ")}>
-                    <div className={'SpecsList__Spec__Header'}>
-                        <div>{spec}</div>
-                        <div>Passed: {stats[spec].pass}/{stats[spec].total}</div>
-                    </div>
-                    <div>
-                        {JSON.stringify(getTestListForSpec(stats[spec]))}
-                    </div>
-                </li>)
+                    (<li key={`suite-${index}`}
+                         className={['SpecsList__Spec', stats[spec].fail > 0 ? 'error' : ''].join(" ")}>
+                        <div className={'SpecsList__Spec__Header'}>
+                            <div>{spec}</div>
+                            <div>Passed: {stats[spec].pass}/{stats[spec].total}</div>
+                        </div>
+                        <ul className='SpecsList__Spec__TestList'>
+                            {getTestListForSpec(stats[spec]).map(
+                                test => (
+                                    <li className={['SpecsList__Spec__TestList__Test', test.passed ? '' : 'error'].join(" ")}>
+                                    <span className={["material-icons", test.passed ? '' : 'error'].join(' ')}>
+                                        {test.passed ? 'check' : 'close'}
+                                        </span>{test.title}
+                                    </li>)
+                            )}
+                        </ul>
+                    </li>)
             )}
         </ul>
     )

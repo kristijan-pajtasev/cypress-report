873c21cd8bf078beb16926d091d05de953e0bdf9
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-04 01:37:33 +0100

    get tests list

--- src/components/SpecsList/SpecsList.js
+++ src/components/SpecsList/SpecsList.js
@@ -3,12 +3,33 @@ import './SpecsList.css';
 
 const SpecsList = (props) => {
     const {specs, stats} = props;
+
+    const getTestListForSpec = (spec, tests = []) => {
+        if(spec.tests && spec.tests.length > 0) tests = [...tests, spec.tests.map(test => ({
+            title: test.fullTitle,
+            passed: test.passed
+        }))]
+
+        const suites = Object.keys(spec).filter(key => !["tests", "pass", "fail", "total", "title", "fullTitle"].includes(key));
+        if(suites.length > 0) {
+            suites.forEach(suite => {
+                const testsForSuite = getTestListForSpec(spec[suite]);
+                tests = [...tests, ...testsForSuite];
+            })
+        }
+
+        return tests.flat();
+    }
+
     return (
         <ul>
             {specs.map((spec, index) =>
                 (<li key={`suite-${index}`} className={stats[spec].fail > 0 ? 'error' : ''}>
                     <div>{spec}</div>
                     <div>Passed: {stats[spec].pass}/{stats[spec].total}</div>
+                    <div>
+                        {JSON.stringify(getTestListForSpec(stats[spec]))}
+                    </div>
                 </li>)
             )}
         </ul>

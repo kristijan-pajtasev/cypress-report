78965117e11eae4a123a3303307aaf2c8dd13b89
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-05 00:07:58 +0100

    test list

--- src/components/TestList/TestList.js
+++ src/components/TestList/TestList.js
@@ -2,9 +2,46 @@ import React from 'react';
 import './TestList.css';
 
 const TestList = (props) => {
-    console.log(props)
+    const {spec, specName} = props;
+
+    const getTestListForSpec = (spec, tests = []) => {
+        if (spec.tests && spec.tests.length > 0) tests = [...tests, spec.tests.map(test => ({
+            title: test.fullTitle,
+            passed: test.passed
+        }))]
+
+        const suites = Object.keys(spec).filter(key => !["tests", "pass", "fail", "total", "title", "fullTitle"].includes(key));
+        if (suites.length > 0) {
+            suites.forEach(suite => {
+                const testsForSuite = getTestListForSpec(spec[suite]);
+                tests = [...tests, ...testsForSuite];
+            })
+        }
+
+        return tests.flat();
+    }
+
     return (
-        <div>test list</div>
+        <div className='TestList'>
+                <div
+                     className={['TestList__Spec', spec.fail > 0 ? 'error' : ''].join(" ")}>
+                    <div className={'TestList__Spec__Header'}>
+                        <div>{specName}</div>
+                        <div>Passed: {spec.pass}/{spec.total}</div>
+                    </div>
+                    <ul className='TestList__Spec__TestList'>
+                        {getTestListForSpec(spec).map(
+                            (test, index) => (
+                                <li key={`suite-${index}-${test.title}`} className={['TestList__Spec__TestList__Test', test.passed ? '' : 'error'].join(" ")}>
+                                <span className={["material-icons", test.passed ? '' : 'error'].join(' ')}>
+                                    {test.passed ? 'check' : 'close'}
+                                    </span>
+                                    {test.title}
+                                </li>)
+                        )}
+                    </ul>
+                </div>
+        </div>
     )
 }
 

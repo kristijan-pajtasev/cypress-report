502299afe8aee621567ab3887c51cb7536e523bd
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-11 01:30:51 +0100

    set next spec index

--- src/components/TestsContainer/TestsContainer.js
+++ src/components/TestsContainer/TestsContainer.js
@@ -7,10 +7,16 @@ const TestsContainer = (props) => {
     const numOfElements = specs.length;
     const [firstIndex, setFirstIndex] = useState(0);
     const [secondIndex, setSecondIndex] = useState(1);
+
+    const showNextSpec = (setFunction) => {
+        const nextIndex = (Math.max(firstIndex, secondIndex) + 1) % numOfElements;
+        setFunction(nextIndex);
+    }
+
     return (
         <div className="TestsContainer">
-            {numOfElements > 0 && <TestList spec={stats[specs[firstIndex]]} specName={specs[firstIndex]}/>}
-            {numOfElements > 1 && <TestList spec={stats[specs[secondIndex]]} specName={specs[secondIndex]}/>}
+            {numOfElements > 0 && <TestList onEnd={showNextSpec.bind(null, setFirstIndex)} spec={stats[specs[firstIndex]]} specName={specs[firstIndex]}/>}
+            {numOfElements > 1 && <TestList onEnd={showNextSpec.bind(null, setSecondIndex)} spec={stats[specs[secondIndex]]} specName={specs[secondIndex]}/>}
         </div>
     )
 

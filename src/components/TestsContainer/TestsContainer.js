2d3b82028a02a0b85cd39b8e1e0aad89fa086cc0
Kristijan <kristijan.pajtasev@hotmail.com>
2020-06-11 01:27:43 +0100

    use state variables for displayed spec

--- src/components/TestsContainer/TestsContainer.js
+++ src/components/TestsContainer/TestsContainer.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TestsContainer.css';
 import TestList from "../TestList";
 
 const TestsContainer = (props) => {
     const {specs, stats} = props;
-
+    const numOfElements = specs.length;
+    const [firstIndex, setFirstIndex] = useState(0);
+    const [secondIndex, setSecondIndex] = useState(1);
     return (
         <div className="TestsContainer">
-            <TestList spec={stats[specs[0]]} specName={specs[0]}/>
-            <TestList spec={stats[specs[1]]} specName={specs[1]}/>
+            {numOfElements > 0 && <TestList spec={stats[specs[firstIndex]]} specName={specs[firstIndex]}/>}
+            {numOfElements > 1 && <TestList spec={stats[specs[secondIndex]]} specName={specs[secondIndex]}/>}
         </div>
     )
 
